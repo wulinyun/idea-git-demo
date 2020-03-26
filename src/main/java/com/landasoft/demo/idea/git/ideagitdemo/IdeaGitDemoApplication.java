61b36a6a0386179d20be9e5a5720595cfeaa8cd3
wulinyun <lin.wu@landasoft.com>
2020-03-27 02:02:32 +0800

    idea-git-demo操作示例

---
+++ src/main/java/com/landasoft/demo/idea/git/ideagitdemo/IdeaGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.idea.git.ideagitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaGitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/landasoft/demo/idea/git/ideagitdemo/IdeaGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.idea.git.ideagitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaGitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
