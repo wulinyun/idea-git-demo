e3e21dfcf79cd4b8ba0f6e8ca45d1fcd705ab5b5
wulinyun <lin.wu@landasoft.com>
2020-03-27 02:12:36 +0800

    测试新增代码并提交无冲突

--- src/main/java/com/landasoft/demo/idea/git/ideagitdemo/IdeaGitDemoApplication.java
+++ src/main/java/com/landasoft/demo/idea/git/ideagitdemo/IdeaGitDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IdeaGitDemoApplication {
 
     public static void main(String[] args) {
+        //测试新增代码并提交
         SpringApplication.run(IdeaGitDemoApplication.class, args);
     }
 
