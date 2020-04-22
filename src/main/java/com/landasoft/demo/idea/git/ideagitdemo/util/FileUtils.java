c63408dc894548568186f137f96d2fa288c52767
wulinyun <lin.wu@landasoft.com>
2020-04-22 14:51:03 +0800

    增加通过图片文件获取图片类型方法

--- src/main/java/com/landasoft/demo/idea/git/ideagitdemo/util/FileUtils.java
+++ src/main/java/com/landasoft/demo/idea/git/ideagitdemo/util/FileUtils.java
@@ -1,8 +1,13 @@
 package com.landasoft.demo.idea.git.ideagitdemo.util;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
 
 /**
  * @Author wulinyun
@@ -25,13 +30,40 @@ public class FileUtils {
         byte[] data =new byte[BUFFER_SIZE];
         int count = -1;
 
-        while((count = in.read(data,0,BUFFER_SIZE)) != -1)
+        while((count = in.read(data,0,BUFFER_SIZE)) != -1){
             outStream.write(data,0, count);
-
+        }
         data =null;
         byte[] outByte = outStream.toByteArray();
         outStream.close();
 
         return outByte;
+    }
+
+    /**
+     * 通过图片文件获取图片类型
+     * @param f
+     * @return
+     */
+    public static String getFormatInFile(File f){
+        try {
+            //Create an image input stream on the image 
+            ImageInputStream iis = ImageIO.createImageInputStream(f);
+            //Find all image readers that recognize the image format
+            Iterator<ImageReader> iter = ImageIO.getImageReaders(iis);
+            if(!iter.hasNext()){
+                // No readers found 
+                return null;
+            }
+            //Use the first reader
+            ImageReader reader = iter.next();
+            //close stream
+            iis.close();
+            //Return the format name
+            return reader.getFormatName();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
