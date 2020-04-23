51e0ff75e46fd83a15d424fadea6d0615d496265
wulinyun <lin.wu@landasoft.com>
2020-04-23 13:26:20 +0800

    微信APIv3回调通知解密算法工具

---
+++ src/main/java/com/landasoft/demo/idea/git/ideagitdemo/util/WxAPIV3AesUtils.java
@@ -0,0 +1,51 @@
+package com.landasoft.demo.idea.git.ideagitdemo.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @JdkVesion 1.7
+ * @Description 微信APIv3回调通知解密算法工具类
+ * @Date 2020/4/23 13:09
+ */
+public class WxAPIV3AesUtils {
+    static final int KEY_LENGTH_BYTE = 32;
+    static final int TAG_LENGTH_BIT = 128;
+    private final byte[] aesKey;
+
+    public WxAPIV3AesUtils(byte[] key) {
+        if (key.length != KEY_LENGTH_BYTE) {
+            throw new IllegalArgumentException("无效的ApiV3Key，长度必须为32个字节");
+        }
+        this.aesKey = key;
+    }
+
+    public String decryptToString(byte[] associatedData, byte[] nonce, String ciphertext)
+            throws GeneralSecurityException, IOException {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
+            SecretKeySpec key = new SecretKeySpec(aesKey, "AES");
+            GCMParameterSpec spec = new GCMParameterSpec(TAG_LENGTH_BIT, nonce);
+
+            cipher.init(Cipher.DECRYPT_MODE, key, spec);
+            cipher.updateAAD(associatedData);
+
+            return new String(cipher.doFinal(Base64.getDecoder().decode(ciphertext)), "utf-8");
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new IllegalStateException(e);
+        } catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
