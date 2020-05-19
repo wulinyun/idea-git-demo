dd2c91dedb506d9db3cd6bfaedd65b7ee68ed81f
wulinyun <lin.wu@landasoft.com>
2020-05-19 17:50:29 +0800

    测试修改dev

--- src/main/java/com/landasoft/demo/idea/git/ideagitdemo/IdeaGitDemoApplication.java
+++ src/main/java/com/landasoft/demo/idea/git/ideagitdemo/IdeaGitDemoApplication.java
@@ -9,6 +9,7 @@ public class IdeaGitDemoApplication {
     public static void main(String[] args) {
         //测试新增代码冲突提交11111111111
         //测试新增代码并提交2222222222222211111111111111111111
+        //测试dev修改
         SpringApplication.run(IdeaGitDemoApplication.class, args);
     }
 
